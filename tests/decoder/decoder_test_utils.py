fed4999e651b5be66f9d5ea8b786738a9e63c264
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2019-06-12 08:27:47 +0100

    Add parse_info offset auto-fill utility.

--- tests/decoder/decoder_test_utils.py
+++ tests/decoder/decoder_test_utils.py
@@ -2,6 +2,7 @@ from io import BytesIO
 
 from vc2_conformance import bitstream
 from vc2_conformance import decoder
+from vc2_conformance import tables
 
 from vc2_conformance.state import State
 
@@ -29,3 +30,50 @@ def bytes_to_state(bitstream):
     state = State()
     decoder.init_io(state, f)
     return state
+
+
+def populate_parse_offsets(sequence, state=None):
+    """
+    Given a complete :py:class:`vc2_conformance.bitstream.Sequence`, update all
+    of the parse_info next_parse_offset and previous_parse_offset values to the
+    correct value.
+    """
+    state = state.copy() if state is not None else State()
+    w = bitstream.BitstreamWriter(BytesIO())
+    
+    # This function seriallises the provided sequence using a
+    # MonitoredSerialiser to log the offsets at which the parse_info headers
+    # are written.
+    parse_info_offsets = []
+    
+    def capture_parse_info_offsets(serdes, target, value):
+        if target == "parse_info_prefix":
+            # NB: Offset reported is at the end of the parse_info_prefix so the
+            # parse_info block started 32 bits earlier.
+            next_byte, next_bit = w.tell()
+            assert next_bit == 7
+            parse_info_offsets.append(next_byte - 4)
+    
+    with bitstream.MonitoredSerialiser(
+        capture_parse_info_offsets,
+        w,
+        sequence,
+        bitstream.vc2_default_values,
+    ) as serdes:
+        bitstream.parse_sequence(serdes, state)
+    
+    # Retrospectively update the parse_info offsets in-place
+    for i in range(len(sequence["data_units"])):
+        if i == 0:
+            sequence["data_units"][i]["parse_info"]["previous_parse_offset"] = 0
+        else:
+            sequence["data_units"][i]["parse_info"]["previous_parse_offset"] = (
+                parse_info_offsets[i] - parse_info_offsets[i-1]
+            )
+        
+        if i == len(sequence["data_units"]) - 1:
+            sequence["data_units"][i]["parse_info"]["next_parse_offset"] = 0
+        else:
+            sequence["data_units"][i]["parse_info"]["next_parse_offset"] = (
+                parse_info_offsets[i+1] - parse_info_offsets[i]
+            )
---
+++ tests/decoder/test_decoder_test_utils.py
@@ -0,0 +1,66 @@
+import pytest
+
+from decoder_test_utils import populate_parse_offsets, serialise_to_bytes
+
+from vc2_conformance import bitstream
+from vc2_conformance import tables
+
+
+class TestPopulateParseOffsets(object):
+    
+    def test_empty_sequence(self):
+        seq = bitstream.Sequence(data_units=[
+            bitstream.DataUnit(
+                parse_info=bitstream.ParseInfo(
+                    parse_code=tables.ParseCodes.end_of_sequence,
+                ),
+            ),
+        ])
+        
+        populate_parse_offsets(seq)
+        
+        assert seq["data_units"][0]["parse_info"]["next_parse_offset"] == 0
+        assert seq["data_units"][0]["parse_info"]["previous_parse_offset"] == 0
+    
+    def test_empty_sequence(self):
+        sh = bitstream.SequenceHeader()
+        
+        seq = bitstream.Sequence(data_units=[
+            bitstream.DataUnit(
+                parse_info=bitstream.ParseInfo(
+                    parse_code=tables.ParseCodes.sequence_header,
+                ),
+                sequence_header=sh,
+            ),
+            bitstream.DataUnit(
+                parse_info=bitstream.ParseInfo(
+                    parse_code=tables.ParseCodes.sequence_header,
+                ),
+                sequence_header=sh,
+            ),
+            bitstream.DataUnit(
+                parse_info=bitstream.ParseInfo(
+                    parse_code=tables.ParseCodes.end_of_sequence,
+                ),
+            ),
+        ])
+        
+        sh_length = len(serialise_to_bytes(sh))
+        populate_parse_offsets(seq)
+        
+        assert seq["data_units"][0]["parse_info"]["next_parse_offset"] == (
+            tables.PARSE_INFO_HEADER_BYTES + sh_length
+        )
+        assert seq["data_units"][0]["parse_info"]["previous_parse_offset"] == 0
+        
+        assert seq["data_units"][1]["parse_info"]["next_parse_offset"] == (
+            tables.PARSE_INFO_HEADER_BYTES + sh_length
+        )
+        assert seq["data_units"][1]["parse_info"]["previous_parse_offset"] == (
+            tables.PARSE_INFO_HEADER_BYTES + sh_length
+        )
+        
+        assert seq["data_units"][2]["parse_info"]["next_parse_offset"] == 0
+        assert seq["data_units"][2]["parse_info"]["previous_parse_offset"] == (
+            tables.PARSE_INFO_HEADER_BYTES + sh_length
+        )
