55d3c59c03487615512ba04961077ebf714eb538
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-03-02 11:39:59 +0000

    Remove old test-case logic.

--- vc2_conformance/test_cases/__init__.py
+++ vc2_conformance/test_cases/__init__.py
@@ -5,9 +5,3 @@
 This module contains a collection of routines for generating test cases for
 VC-2 encoders and decoders.
 """
-
-# Import all of the test-case containing modules to ensure all test cases are
-# registered with the registry.
-from vc2_conformance.test_cases import cases_metadata
-
-from vc2_conformance.test_cases.registry import test_case_registry, XFail
