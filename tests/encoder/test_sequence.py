17e66297975fdd12fe47a00755e2fc4ae7b05cba
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-03-09 11:19:17 +0000

    Add make_sequence function.

---
+++ tests/encoder/test_sequence.py
@@ -0,0 +1,147 @@
+import pytest
+
+import os
+
+import sys
+
+from io import BytesIO
+
+from vc2_data_tables import (
+    Profiles,
+    Levels,
+    ParseCodes,
+)
+
+from vc2_conformance.encoder.sequence import make_sequence
+
+from vc2_conformance.picture_generators import mid_gray
+
+from vc2_conformance.bitstream import autofill_and_serialise_sequence
+
+from vc2_conformance.level_constraints import (
+    LEVEL_SEQUENCE_RESTRICTIONS,
+    LevelSequenceRestrictions,
+)
+
+# Add parent directory to path for sample_codec_features test utility module
+sys.path.append(os.path.join(
+    os.path.dirname(__file__),
+    "..",
+))
+
+from sample_codec_features import MINIMAL_CODEC_FEATURES
+
+from vc2_conformance.state import State
+
+from vc2_conformance.decoder import (
+    init_io,
+    parse_sequence,
+)
+
+
+def serialize_and_decode(sequence):
+    # Serialise
+    f = BytesIO()
+    autofill_and_serialise_sequence(f, sequence)
+    
+    # Setup callback to capture decoded pictures
+    decoded_pictures = []
+    def output_picture_callback(picture, video_parameters):
+        decoded_pictures.append(picture)
+    
+    # Feed to conformance checking decoder
+    f.seek(0)
+    state = State(_output_picture_callback=output_picture_callback)
+    init_io(state, f)
+    parse_sequence(state)
+    
+    return decoded_pictures
+
+
+def test_make_empty_sequence():
+    seq = make_sequence(MINIMAL_CODEC_FEATURES, [])
+    assert serialize_and_decode(seq) == []
+
+
+@pytest.mark.parametrize("profile,fragment_slice_count", [
+    # High-quality pictures
+    (Profiles.high_quality, 0),
+    # Low-delay pictures and fragmented pictures
+    (Profiles.low_delay, 0),
+    # Fragmented pictures
+    (Profiles.high_quality, 1),
+])
+def test_picture_types(profile, fragment_slice_count):
+    codec_features = MINIMAL_CODEC_FEATURES.copy()
+    codec_features["profile"] = profile
+    codec_features["fragment_slice_count"] = fragment_slice_count
+    
+    y, c1, c2 = next(mid_gray(
+        codec_features["video_parameters"],
+        codec_features["picture_coding_mode"],
+    ))
+    pictures = [
+        {"Y": y.tolist(), "C1": c1.tolist(), "C2": c2.tolist(), "pic_num": pic_num}
+        for pic_num in range(100, 104)
+    ]
+    
+    seq = make_sequence(codec_features, pictures)
+    
+    assert serialize_and_decode(seq) == pictures
+
+
+@pytest.yield_fixture
+def patch_unconstratined_level_sequence_restrictions():
+    # For test purposes, modify the unconstrained level so that it requires an
+    # example of every non-picture data unit type to be inserted and that
+    # pictures and sequence headers are interleaved
+    old_value = LEVEL_SEQUENCE_RESTRICTIONS.pop(Levels.unconstrained)
+    
+    new_value = LevelSequenceRestrictions(
+        "Test constraint....",
+        (
+            "sequence_header auxiliary_data "
+            "(sequence_header high_quality_picture)+ "
+            "padding_data end_of_sequence"
+        )
+    )
+    LEVEL_SEQUENCE_RESTRICTIONS[Levels.unconstrained] = new_value
+    
+    try:
+        yield
+    finally:
+        LEVEL_SEQUENCE_RESTRICTIONS[Levels.unconstrained] = old_value
+
+
+def test_level_sequence_restrictions_obeyed(patch_unconstratined_level_sequence_restrictions):
+    y, c1, c2 = next(mid_gray(
+        MINIMAL_CODEC_FEATURES["video_parameters"],
+        MINIMAL_CODEC_FEATURES["picture_coding_mode"],
+    ))
+    pictures = [
+        {"Y": y.tolist(), "C1": c1.tolist(), "C2": c2.tolist(), "pic_num": pic_num}
+        for pic_num in range(100, 104)
+    ]
+    
+    seq = make_sequence(MINIMAL_CODEC_FEATURES, pictures)
+    
+    assert [
+        data_unit["parse_info"]["parse_code"]
+        for data_unit in seq["data_units"]
+    ] == [
+        ParseCodes.sequence_header,
+        ParseCodes.auxiliary_data,
+        ParseCodes.sequence_header,
+        ParseCodes.high_quality_picture,
+        ParseCodes.sequence_header,
+        ParseCodes.high_quality_picture,
+        ParseCodes.sequence_header,
+        ParseCodes.high_quality_picture,
+        ParseCodes.sequence_header,
+        ParseCodes.high_quality_picture,
+        ParseCodes.padding_data,
+        ParseCodes.end_of_sequence,
+    ]
+    
+    # Sanity check
+    assert serialize_and_decode(seq) == pictures
---
+++ vc2_conformance/encoder/sequence.py
@@ -0,0 +1,145 @@
+"""
+Generate complete VC-2 sequences
+================================
+
+This module provides the :py:func:`make_sequence` function which produces
+:py:class:`vc2_conformance.bitstream.Sequence` objects containing pictures
+compressed according to the required codec specifications.
+
+.. autofunction:: make_sequence
+
+Example usage::
+
+    >>> from vc2_conformance.codec_features import CodecFeatures
+    >>> from vc2_conformance.bitstream import autofill_and_serialise_sequence
+    >>> from vc2_conformance.encoder.sequence import make_sequence
+    
+    >>> # Create a Sequence containing a particular set of pictures for a
+    >>> # particular video codec configuration.
+    >>> codec_features = CodecFeatures(...)
+    >>> pictures = [
+    ...     {"Y": ..., "C1": ..., "C2": ..., "pic_num": 100},
+    ...     # ...
+    ... ]
+    >>> sequence = make_sequence(codec_features, pictures)
+    
+    >>> # Serialise to a file
+    >>> with open("bitstream.vc2", "wb") as f:
+    ...     autofill_and_serialise_sequence(f, sequence)
+"""
+
+from functools import partial
+
+from vc2_data_tables import ParseCodes
+
+from vc2_conformance.level_constraints import (
+    LEVEL_SEQUENCE_RESTRICTIONS,
+)
+
+from vc2_conformance.bitstream import (
+    Sequence,
+    DataUnit,
+    ParseInfo,
+)
+
+from vc2_conformance.symbol_re import make_matching_sequence
+
+from vc2_conformance.encoder.sequence_header import (
+    make_sequence_header_data_unit,
+)
+
+from vc2_conformance.encoder.pictures import (
+    make_picture_data_units,
+)
+
+
+__all__ = [
+    "make_sequence",
+]
+
+
+def make_end_of_sequence_data_unit():
+    return DataUnit(
+        parse_info=ParseInfo(parse_code=ParseCodes.end_of_sequence),
+    )
+
+
+def make_auxiliary_data_unit():
+    return DataUnit(
+        parse_info=ParseInfo(parse_code=ParseCodes.auxiliary_data),
+    )
+
+
+def make_padding_data_unit():
+    return DataUnit(
+        parse_info=ParseInfo(parse_code=ParseCodes.padding_data),
+    )
+
+
+def make_sequence(codec_features, pictures, minimum_qindex=0):
+    """
+    Generate a complete VC-2 bitstream based on the provided set of codec
+    features and encoding the specified set of pictures.
+    
+    Parameters
+    ==========
+    codec_features : :py:class:`~vc2_conformance.codec_features.CodecFeatures`
+    pictures : [{"Y": [[s, ...], ...], "C1": ..., "C2": ..., "pic_num": int}, ...]
+        The pictures to be encoded in the bitstream. If ``pic_num`` is omitted,
+        ``picture_number`` fields will be omitted in the output.
+        See :py:mod:`vc2_conformance.encoder.pictures` for details of the
+        picture compression process.
+    minimum_qindex : int
+        Specifies the minimum quantization index to be used. Must be 0 for
+        lossless codecs.
+    
+    Returns
+    =======
+    sequence : :py:class:`vc2_conformance.bitstream.Sequence`
+        The VC-2 bitstream sequence, ready for serialization using
+        :py:func:`~vc2_conformance.bitstream.vc2_autofill.autofill_and_serialise_sequence`.
+    """
+    pictures_only_sequence = Sequence(data_units=[])
+    for picture in pictures:
+        pictures_only_sequence["data_units"].extend(
+            make_picture_data_units(codec_features, picture, minimum_qindex)
+        )
+    
+    # Fill in all other required bitstream data units
+    picture_only_data_unit_names = [
+        data_unit["parse_info"]["parse_code"].name
+        for data_unit in pictures_only_sequence["data_units"]
+    ]
+    required_data_unit_names = make_matching_sequence(
+        picture_only_data_unit_names,
+        # (10.4.1) Sequences start with a sequence header and end with an end
+        # of sequence data unit
+        "sequence_header .* end_of_sequence",
+        # Certain levels may provide additional constraints
+        LEVEL_SEQUENCE_RESTRICTIONS[codec_features["level"]].sequence_restriction_regex,
+        symbol_priority=[
+            "padding_data",
+            "sequence_header",
+        ],
+    )
+    
+    # Functions for producing the necessary data units
+    #
+    # {data_unit_name: fn() -> DataUnit, ...}
+    data_unit_makers = {
+        "sequence_header": partial(make_sequence_header_data_unit, codec_features),
+        "end_of_sequence": make_end_of_sequence_data_unit,
+        "auxiliary_data": make_auxiliary_data_unit,
+        "padding_data": make_padding_data_unit,
+    }
+    if pictures_only_sequence["data_units"]:
+        picture_parse_code = pictures_only_sequence["data_units"][0]["parse_info"]["parse_code"]
+        data_unit_makers[picture_parse_code.name] = partial(
+            pictures_only_sequence["data_units"].pop,
+            0,
+        )
+    
+    return Sequence(data_units=[
+        data_unit_makers[data_unit_name]()
+        for data_unit_name in required_data_unit_names
+    ])
