3565a6c8072b22ef7686e367fb61a8bbbb8afe17
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-03-19 11:50:59 +0000

    Make generic decoder test cover main VC-2 modes.

--- tests/test_cases/decoder/test_all_decoder_test_cases.py
+++ tests/test_cases/decoder/test_all_decoder_test_cases.py
@@ -6,9 +6,13 @@ import sys
 
 from io import BytesIO
 
+from vc2_data_tables import Profiles
+
 from vc2_conformance.bitstream import autofill_and_serialise_sequence
 
 from vc2_conformance.test_cases import DECODER_TEST_CASE_GENERATOR_REGISTRY
+
+from vc2_conformance.codec_features import CodecFeatures
 
 # Add test root directory to path for sample_codec_features test utility module
 sys.path.append(os.path.join(
@@ -28,17 +32,44 @@ from vc2_conformance.decoder import (
 
 
 @pytest.mark.parametrize(
-    "test_case",
-    DECODER_TEST_CASE_GENERATOR_REGISTRY.generate_test_cases(
-        MINIMAL_CODEC_FEATURES,
-    ),
+    "codec_features,test_case",
+    (
+        (codec_features, test_case)
+        for codec_features in [
+            # High quality
+            CodecFeatures(
+                MINIMAL_CODEC_FEATURES,
+                profile=Profiles.high_quality,
+            ),
+            # Low delay
+            CodecFeatures(
+                MINIMAL_CODEC_FEATURES,
+                profile=Profiles.low_delay,
+            ),
+            # Lossless coding
+            CodecFeatures(
+                MINIMAL_CODEC_FEATURES,
+                profile=Profiles.high_quality,
+                lossless=True,
+                picture_bytes=0,
+            ),
+            # Fragmented pictures
+            CodecFeatures(
+                MINIMAL_CODEC_FEATURES,
+                profile=Profiles.high_quality,
+                fragment_slice_count=2,
+            ),
+        ]
+        for test_case in DECODER_TEST_CASE_GENERATOR_REGISTRY.generate_test_cases(
+            codec_features)
+    )
 )
-def test_all_decoder_test_cases(test_case):
-    # Must produce a valid bitstream containing pictures with the correct
-    # format
+def test_all_decoder_test_cases(codec_features, test_case):
+    # Every test case for every basic video mode must produce a valid bitstream
+    # containing pictures with the correct format.
     
     def output_picture_callback(picture, video_parameters):
-        assert video_parameters == MINIMAL_CODEC_FEATURES["video_parameters"]
+        assert video_parameters == codec_features["video_parameters"]
     
     state = State(
         _output_picture_callback=output_picture_callback,
