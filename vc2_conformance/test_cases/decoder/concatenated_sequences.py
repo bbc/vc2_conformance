9b473c3d6d51dc5a9aa15574462d880192fc534c
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2020-05-18 14:33:37 +0100

    Add concatenated_sequences decoder test case.

---
+++ vc2_conformance/test_cases/decoder/concatenated_sequences.py
@@ -0,0 +1,34 @@
+"""
+Tests which verify that codecs correctly process streams formed of multiple
+concatenated sequences (10.3).
+"""
+
+from copy import deepcopy
+
+from vc2_conformance.bitstream import Stream
+
+from vc2_conformance.test_cases import decoder_test_case_generator
+
+from vc2_conformance.picture_generators import mid_gray
+
+from vc2_conformance.encoder import make_sequence
+
+
+@decoder_test_case_generator
+def concatenated_sequences(codec_features):
+    """
+    **Tests that streams containing multiple concatenated sequences can be
+    decoded.**
+
+    A stream consisting of the concatenation two sequences (10.3) with one
+    frame each, the first picture of which is given picture number zero in both
+    sequences.
+    """
+    sequence = make_sequence(
+        codec_features,
+        mid_gray(
+            codec_features["video_parameters"], codec_features["picture_coding_mode"],
+        ),
+    )
+
+    return Stream(sequences=[sequence, deepcopy(sequence)])
