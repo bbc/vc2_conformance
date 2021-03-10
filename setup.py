4f10f67fe56313bd699b6d57d4ec8887d8b2c50a
Jonathan Heathcote <jonathan.heathcote@bbc.co.uk>
2021-03-10 12:26:35 +0000

    Update CI and docs for public release

--- setup.py
+++ setup.py
@@ -45,9 +45,9 @@ setup(
         # due to provision of a binary wheel.
         "bitarray-hardbyte",
         "sentinels",
-        "vc2_data_tables>=0.1.1",
-        "vc2_bit_widths>=0.1.1",
-        "vc2_conformance_data>=0.1.0",
+        "vc2_data_tables==git+https://github.com/bbc/vc2_conformance_data.git@v1.0.0#egg=vc2_conformance_data",
+        "vc2_bit_widths~=1.0.0",
+        "vc2_conformance_data~=1.0.0",
         # Use old versions/polyfill libraries which have deprecated older Python
         # version support
         "enum34; python_version<'3.4'",
